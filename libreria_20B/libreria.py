1dbd84ca308d812a69a68f60636b6e1715e26c34
Emmanuel Paniagua <helaman.paniagua4671@alumnos.udg.mx>
2020-11-04 13:20:09 -0600

    se actualizo para estar a la par con la actividad 12

--- libreria_20B/libreria.py
+++ libreria_20B/libreria.py
@@ -14,14 +14,31 @@ class Libreria:
     def mostrar(self):
         for libro in self.__libros:
             print(libro)
-
+    
     def __str__(self):
         return "".join(
             str(libro) + '\n' for libro in self.__libros
-        )
+        ) 
+
+    def __len__(self):
+        return len(self.__libros)
+
+    def __iter__(self):
+        self.cont = 0
+
+        return self
+
+    def __next__(self):
+        if self.cont < len(self.__libros):
+            libro = self.__libros[self.cont]
+            self.cont += 1
+            return libro
+        else:
+            raise StopIteration
+
 
     def guardar(self, ubicacion):
-        try:
+        try:       
             with open(ubicacion, 'w') as archivo:
                 lista = [libro.to_dict() for libro in self.__libros]
                 print(lista)
@@ -29,7 +46,7 @@ class Libreria:
             return 1
         except:
             return 0
-
+    
     def abrir(self, ubicacion):
         try:
             with open(ubicacion, 'r') as archivo:
@@ -39,11 +56,10 @@ class Libreria:
         except:
             return 0
 
-
-#l01 = Libro(titulo="Programación", autor="Deitel", publicado=2020, editorial="Pearson")
-#l02 = Libro("Python", "Guido", "2010", "Planeta")
-#libreria = Libreria()
-#libreria.agregar_final(l01)
-#libreria.agregar_inicio(l02)
-#libreria.agregar_inicio(l01)
-#libreria.mostrar()
+# l01 = Libro(titulo="Programación", autor="Deitel", publicado=2020, editorial="Pearson")
+# l02 = Libro("Python", "Guido", "2010", "Planeta")
+# libreria = Libreria()
+# libreria.agregar_final(l01)
+# libreria.agregar_inicio(l02)
+# libreria.agregar_inicio(l01)
+# libreria.mostrar()
