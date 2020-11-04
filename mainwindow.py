04fa18057a35423b79ba62e774895873fa925281
Emmanuel Paniagua <helaman.paniagua4671@alumnos.udg.mx>
2020-11-04 13:16:23 -0600

    se actualizo para estar a la par con la act 12

--- mainwindow.py
+++ mainwindow.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QTableWidgetItem
 from PySide2.QtCore import Slot
 from ui_mainwindow import Ui_MainWindow
 from libreria_20B.libreria import Libreria
@@ -14,14 +14,69 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.ui.agregar_final_pushButton.clicked.connect(self.click_agregar)
         self.ui.agregar_inicio_pushButton.clicked.connect(self.click_agregar_inicio)
-        self.ui.mostrar_pushButton.clicked.connect(self.click_mostrar)
+        self.ui.monstrar_pushButton.clicked.connect(self.click_mostrar)
 
         self.ui.actionAbrir.triggered.connect(self.action_abrir_archivo)
         self.ui.actionGuardar.triggered.connect(self.action_guardar_archivo)
 
+        self.ui.mostrar_tabla_pushButton.clicked.connect(self.mostrar_tabla)
+        self.ui.buscar_pushButton.clicked.connect(self.buscar_titulo)
+
+    @Slot()
+    def buscar_titulo(self):
+        titulo = self.ui.buscar_lineEdit.text()
+        encontrado = False
+        for libro in self.libreria:
+            if titulo == libro.titulo:
+                self.ui.tabla.clear()
+                self.ui.tabla.setRowCount(1)
+
+                titulo_widget = QTableWidgetItem(libro.titulo)
+                autor_widget = QTableWidgetItem(libro.autor)
+                publicado_widget = QTableWidgetItem(str(libro.publicado))
+                editorial_widget = QTableWidgetItem(libro.editorial)
+
+                self.ui.tabla.setItem(0, 0, titulo_widget)
+                self.ui.tabla.setItem(0, 1, autor_widget)
+                self.ui.tabla.setItem(0, 2, publicado_widget)
+                self.ui.tabla.setItem(0, 3, editorial_widget)
+
+                encontrado = True
+                return
+        if not encontrado:
+            QMessageBox.warning(
+                self,
+                "Atención",
+                f'El libro con título "{titulo}" no fue encontrado'
+            )
+        
+
+
+    @Slot()
+    def mostrar_tabla(self):
+        self.ui.tabla.setColumnCount(4)
+        headers = ["Título", "Autor", "Publicado", "Editorial"]
+        self.ui.tabla.setHorizontalHeaderLabels(headers)
+
+        self.ui.tabla.setRowCount(len(self.libreria))
+
+        row = 0
+        for libro in self.libreria:
+            titulo_widget = QTableWidgetItem(libro.titulo)
+            autor_widget = QTableWidgetItem(libro.autor)
+            publicado_widget = QTableWidgetItem(str(libro.publicado))
+            editorial_widget = QTableWidgetItem(libro.editorial)
+
+            self.ui.tabla.setItem(row, 0, titulo_widget)
+            self.ui.tabla.setItem(row, 1, autor_widget)
+            self.ui.tabla.setItem(row, 2, publicado_widget)
+            self.ui.tabla.setItem(row, 3, editorial_widget)
+
+            row += 1
+
     @Slot()
     def action_abrir_archivo(self):
-        #print('abrir_archivo')
+        # print('abrir_archivo')
         ubicacion = QFileDialog.getOpenFileName(
             self,
             'Abrir Archivo',
@@ -32,41 +87,42 @@ class MainWindow(QMainWindow):
             QMessageBox.information(
                 self,
                 "Éxito",
-                "Se abrió el archivo" + ubicacion
-            )
-        else:
-            QMessageBox.information(
-                self,
-                "Error"
-                "Error al abrir el archivo" + ubicacion
-            )
-
-    @Slot()
-    def action_guardar_archivo(self):
-        #print('guardar_archivo')
-        ubicacion = QFileDialog.getSaveFileName(
-            self,
-            'Guardar Archivo',
-            '.',
-            'JSON (*.json)'
-        ) [0]
-        print(ubicacion)
-        if self.libreria.guardar(ubicacion):
-            QMessageBox.information(
-                self,
-                "Éxito",
-                "Se pudo crear el archivo" + ubicacion
+                "Se abrió el archivo " + ubicacion
             )
         else:
             QMessageBox.critical(
                 self,
                 "Error",
-                "No se pudo crear el archivo" + ubicacion
+                "Error al abrir el archivo " + ubicacion
             )
 
     @Slot()
+    def action_guardar_archivo(self):
+        # print('guardar_archivo')
+        ubicacion = QFileDialog.getSaveFileName(
+            self,
+            'Guardar Archivo',
+            '.',
+            'JSON (*.json)'
+        )[0]
+        print(ubicacion)
+        if self.libreria.guardar(ubicacion):
+            QMessageBox.information(
+                self,
+                "Éxito",
+                "Se pudo crear el archivo " + ubicacion
+            )
+        else:
+            QMessageBox.critical(
+                self,
+                "Error",
+                "No se pudo crear el archivo " + ubicacion
+            )
+
+
+    @Slot()
     def click_mostrar(self):
-        #self.libreria.mostrar()
+        # self.libreria.mostrar()
         self.ui.salida.clear()
         self.ui.salida.insertPlainText(str(self.libreria))
 
@@ -80,9 +136,8 @@ class MainWindow(QMainWindow):
         libro = Libro(titulo, autor, publicado, editorial)
         self.libreria.agregar_final(libro)
 
-        #print(titulo, autor, publicado, editorial)
-        #self.ui.salida.insertPlainText(titulo + autor + str(publicado) + editorial)
-    
+        # print(titulo, autor, publicado, editorial)
+        # self.ui.salida.insertPlainText(titulo + autor + str(publicado) + editorial)
     @Slot()
     def click_agregar_inicio(self):
         titulo = self.ui.titulo_lineEdit.text()
